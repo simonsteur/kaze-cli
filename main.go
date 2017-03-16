c191cbb392aade4626244ac24c02676c9adaa36e
simons <simon@sinch.com>
2017-03-16 18:56:57 +0100

    adding command flags

--- main.go
+++ main.go
@@ -12,30 +12,66 @@ var (
 	checksapi = apibase + "/checks"
 
 	// client flag vars
-	client              bool
-	checks              bool
-	events              bool
-	silence             bool
-	results             bool
-	name                string
-	clientBulk          bool
-	clientBulkFile      string
-	clientAddress       string
-	clientEnvironment   string
-	clientSubscriptions []string
+	client    bool
+	checks    bool
+	events    bool
+	silence   bool
+	results   bool
+	aggregate bool
+	stash     bool
+	name      []string
+	bulkFile  string
+	// silence command specific
+	listSilenced         bool
+	clearSilenced        bool
+	subscriptionSilenced bool
 )
 
 func main() {
 
+	//list command
 	listCmd := flag.NewFlagSet("list", flag.ExitOnError)
-	listCmd.BoolVar(&client, "client", false, "specify to list clients")
-	listCmd.BoolVar(&checks, "checks", false, "specify to list checks")
-	listCmd.BoolVar(&events, "events", false, "specify to list events")
+	listCmd.BoolVar(&client, "client", false, "use to list client(s)")
+	listCmd.BoolVar(&check, "checks", false, "use to list check(s)")
+	listCmd.BoolVar(&event, "events", false, "use to list event(s)")
+	listCmd.BoolVar(&silence, "silence", false, "use to list silence entr(y)(ies)")
+	listCmd.BoolVar(&result, "result", false, "use to list result(s)")
+	listCmd.BoolVar(&aggregate, "aggregate", false, "luse to ist aggregate(s)")
+	istCmd.BoolVar(&stash, "stash", false, "use to list stash(es)")
+	listCmd.StringVar(&name, "name", "", "specify the name(s) of the object(s) to list")
 
-	// error message
+	//create command
+	createCmd := flag.NewFlagSet("create", flag.ExitOnError)
+	createCmd.BoolVar(&client, "client", false, "use to create client(s)")
+	createCmd.BoolVar(&result, "result", false, "use to create result(s)")
+	createCmd.BoolVar(&stash, "stash", false, "use to create stash(es)")
+	createCmd.BoolVar(&bulkFile, "file", false, "a valid json file for creation of objects")
+
+	//delete command
+	deleteCmd := flag.NewFlagSet("delete", flag.ExitOnError)
+	listCmd.BoolVar(&client, "client", false, "use to delete client(s)")
+	listCmd.BoolVar(&event, "events", false, "use to delete event(s)")
+	listCmd.BoolVar(&result, "result", false, "use to delete result(s)")
+	listCmd.BoolVar(&aggregate, "aggregate", false, "use to delete aggregate(s)")
+	istCmd.BoolVar(&stash, "stash", false, "use to delete stash(es)")
+	listCmd.StringVar(&name, "name", "", "specify the name(s) of the object(s) to delete")
+
+	//silence command
+	silenceCmd := flag.NewFlagSet("silence", flag.ExitOnError)
+	listCmd.BoolVar(&clear, "clear", false, "use to clear silenced entr(y)(ies)")
+	listCmd.BoolVar(&list, "list", false, "use to list silenced entr(y(ies)")
+	listCmd.BoolVar(&client, "client", false, "use to target client(s)")
+	listCmd.BoolVar(&check, "subscription", false, "use to target subscription(s)")
+	listCmd.StringVar(&name, "name", "", "specify the name(s) of the client(s) or subscription(s)")
 
 	switch os.Args[1] {
 	case "list":
+		listCmd.Parse(os.Args[2:])
+	case "create":
+		listCmd.Parse(os.Args[2:])
+	case "delete":
+		listCmd.Parse(os.Args[2:])
+	case "silence":
 		listCmd.Parse(os.Args[2:])
 	default:
 		flag.PrintDefaults()
@@ -43,7 +79,7 @@ func main() {
 	}
 
 	if listCmd.Parsed() {
-		handleListCmd(client, checks, events)
+		handleListCmd(client, check, event, silence, result, aggregate, stash)
 	}
 
 	// app := cli.NewApp()
